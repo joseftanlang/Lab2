324205f6b0219113de34cda6dc26691a8956cb19
Josef Tan <JOSEFTAN.25@ichat.sp.edu.sg>
2025-05-08 17:37:56 +0800

    'BMI'

---
+++ BMI.py
@@ -0,0 +1,9 @@
+print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def calculate_bmi(height, weight): 
+    print("Height = " + height) 
+    print("Weight = " + weight) 
+calculate_bmi(weight="57", height="1.73")
+
+print("BMI = " + str(57 / (1.73 * 1.73)))
